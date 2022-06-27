f670da99531ecf6094f3ade1c1f59f8062aa23c0
emmegray <lunaienuso@libero.it>
2022-06-27 17:50:36 +0200

    Removed second from date of trains

--- database/seeds/TrainTableSeeder.php
+++ database/seeds/TrainTableSeeder.php
@@ -13,13 +13,17 @@ class TrainTableSeeder extends Seeder
      */
     public function run(Faker $faker)
     {
-        for ($i = 0; $i < 100; $i++) {
+        for ($i = 0; $i < 50; $i++) {
             $new_train = new Train();
+
+            $partenza = date("Y-m-d H:i:00", strtotime($faker->time()));
+            $arrivo = date("Y-m-d H:i:00", strtotime($faker->time()));
+
             $new_train->azienda = $faker->company();
             $new_train->stazione_di_partenza = $faker->streetAddress();
             $new_train->stazione_di_arrivo = $faker->streetAddress();
-            $new_train->orario_partenza = $faker->time();
-            $new_train->orario_di_arrivo = $faker->time();
+            $new_train->orario_partenza = $partenza;
+            $new_train->orario_di_arrivo = $arrivo;
             $new_train->codice_treno = $faker->numberBetween(1, 50);
             $new_train->numero_carrozze = $faker->numberBetween(1, 10);
             $new_train->in_orario = $faker->boolean();
