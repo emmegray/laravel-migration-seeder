d4176a980e01ce1af934444c390f5f1c6ad6e148
Manuela Ienuso <43219535+emmegray@users.noreply.github.com>
2022-06-27 13:49:48 +0200

    Created base Laravel project

---
+++ app/Http/Controllers/PageController.php
@@ -0,0 +1,15 @@
+<?php
+
+use Faker\Guesser\Name;
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PageController extends Controller
+{
+    function index()
+    {
+        return view('home');
+    }
+}
