5567a92f56db58e90a28664d02e14b96bcb252b3
Manuela Ienuso <43219535+emmegray@users.noreply.github.com>
2022-06-27 14:22:16 +0200

    Changed Database, TrainTable Seeder

--- database/seeds/TrainTableSeeder.php
+++ database/seeds/TrainTableSeeder.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Train;
 use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
 
 class TrainTableSeeder extends Seeder
 {
@@ -9,8 +11,19 @@ class TrainTableSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        //
+        for ($i=0; $i < 100; $i++) {
+            $new_train = new Train();
+            $new_train->azienda = $faker->company();
+            $new_train->stazione_di_partenza = $faker->streetAddress();
+            $new_train->stazione_di_arrivo = $faker->streetAddress();
+            $new_train->orario_di_partenza = $faker->time();
+            $new_train->orario_di_arrivo = $faker->time();
+            $new_train->codice_treno = $faker->numberBetween(1,50);
+            $new_train->numero_carrozze = $faker->numberBetween(1,10);
+            $new_train->in_orario = $faker->numberBetween(0,1);
+            $new_train->cancellato = $faker->numberBetween(0,1);
+        }
     }
 }
