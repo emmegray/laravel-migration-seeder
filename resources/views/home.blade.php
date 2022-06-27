8d5c236466f6840b6b39fe3c66d4422d09a9583c
emmegray <lunaienuso@libero.it>
2022-06-27 17:16:29 +0200

    Added simple table

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,14 +3,31 @@
 @section('content')
 
 <div class="container px-4 py-5">
-  <h2 class="pb-2 border-bottom">Trains</h2>
+  <h2 class="pb-2 border-bottom">🚂 Treni</h2>
 
   <div class="row g-4 py-5">
-    <ul>
-      @foreach ($trains as $train)
-      <li>{{$train->id}} - {{$train->stazione_di_partenza}} - {{$train->stazione_di_arrivo}}</li>
-      @endforeach
-    </ul>
+
+    <table class="table">
+      <thead>
+        <tr>
+          <th scope="col">Codice Treno:</th>
+          <th scope="col">Stazione di Partenza:</th>
+          <th scope="col">Stazione di Arrivo:</th>
+          <th scope="col">Orario di arrivo:</th>
+        </tr>
+      </thead>
+      <tbody>
+        @foreach ($trains as $train)
+        <tr>
+          <th scope="row">{{$train->codice_treno}}</th>
+          <td>{{$train->stazione_di_partenza}}</td>
+          <td>{{$train->stazione_di_arrivo}}</td>
+          <td>{{$train->orario_di_arrivo}}</td>
+        </tr>
+        @endforeach
+      </tbody>
+    </table>
+
   </div>
 
 </div>
